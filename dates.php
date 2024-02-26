4571591c4c36e997d6188ffd02c48975846aa7fb
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-02-26 15:15:29 +0530

    dates

--- dates.php
+++ dates.php
@@ -1,3 +1,8 @@
 <?php
-echo date('y-m-d');
+echo date('y-m-d H-i-s');
+// yesterday
+echo date('y-m-d H:i:s', time()-60*60*24).'<br>';
+
+$parsedate = date_parse('2024-3-12 09:00:00');
+var_dump($parsedate);
 ?>
