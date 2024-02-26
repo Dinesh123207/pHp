db52ef5cf09508e4f8536c731949fb8925832e49
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-02-26 23:07:30 +0530

    ...

--- website_skeleton/about.php
+++ website_skeleton/about.php
@@ -1,7 +1,4 @@
 <?php include "partials/header.php";?>
 <!-- <?php require "partials/header.php";?> -->
 <h1>About us</h1>
-<footer>
-    Copyright &copy; TheCodeholic</footer> 
-</body>
-</html>
+<?php include "partials/footer.php";?>
--- website_skeleton/index.php
+++ website_skeleton/index.php
@@ -20,8 +20,4 @@
 </header> -->
 <h3>Georgia, Tbilisi 5&#8451;</h3>
 <h1>Welcome to my cool website</h1>
-<footer>
-    Copyright &copy; 2020 TheCodeholic
-</footer>
-</body>
-</html>
+<?php include "partials/footer.php";?>
