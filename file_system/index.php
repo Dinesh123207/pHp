580b2bc902f42804d125e6eebfaf649b31bbb69e
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-02-28 11:25:10 +0530

    important

--- file_system/index.php
+++ file_system/index.php
@@ -1,21 +1,28 @@
 <?php
 // Magic constants
-
+echo __DIR__;
+echo __FILE__;
+echo __LINE__;
 
 // Create directory
-
+mkdir('test');
 
 // Rename directory
+rename('test','new_test');
 
 
 // Delete directory
-
+rmdir('new_test');
 
 // Read files and folders inside directory
+echo file_get_contents('lorem.txt');
 
 // file_get_contents, file_put_contents
+$file = scandir('./'); 
 
 // file_get_contents from URL
+$userJson = file_get_contents('https://www.dronanotes.live/');
+echo $userJson;
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
